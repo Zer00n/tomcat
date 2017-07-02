60b949ec364686cd894f8e48cd6bcc98bb2ec3b1
Zer00n <41852518@qq.com>
2017-07-02 16:17:01 +0800

    Update devops.sh

--- devops.sh
+++ devops.sh
@@ -8,17 +8,20 @@ cd /home/stms/
 rm -rf stms/
 git clone http://172.16.0.11:8000/lishilei/stms.git
 
+echo "#############clean images################"
+CONTAINER=`docker ps | grep ${PROJECT} | awk '{print $1}'`
+docker stop ${CONTAINER}
+docker rm ${CONTAINER}
+#CONTAINER2=`docker images | grep ${PROJECT} | awk '{print $3}'`
+#docker rmi ${CONTAINER2}
+
 echo "#################build new images and push to private registry###########"
 
 docker build -t zer00n/stms:${VERSION} .
 
 echo "###########download new images and start new version of project##########"
 
-#CONTAINER="docker ps | grep zer00n/stms | awk '{print $1}'"
-#docker stop ${CONTAINER}
-#docker rm ${CONTAINER}
-docker run -d -p 8090:8080 -v /home/images/logs:/usr/local/web/apache-tomcat-7.0.79/logs  --name=${PROJECT}_${VERSION} zer00n/stms:${VERSION} 
-
+docker run -d -p 8090:8080 -v /home/images/logs:/usr/local/web/apache-tomcat-7.0.79/logs  --name=${PROJECT}_${VERSION} zer00n/stms:${VERSION} 2
 
 
 通过jenkins 自动运行docker容器。
